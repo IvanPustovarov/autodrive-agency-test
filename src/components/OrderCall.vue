47e09291b21445a87824de4bdee6a34edc7d8e70
WIllsnSmith <silverflash150@gmail.com>
2022-08-16 21:12:16 +0300

    order call logic

--- src/components/OrderCall.vue
+++ src/components/OrderCall.vue
@@ -1,11 +1,40 @@
 <template>
   <div v-if="city" class="flex flex-row mb-4 input">
+    <form class="flex flex-col" @submit="submitHandler">
+      <div>
+        <input v-model="name" type="text" placeholder="Иван Иванов" />
+        <input v-model="phone" type="tel" placeholder="+7 . . ." />
+        <input v-model="email" type="email" placeholder="you@example.com" />
+      </div>
+      <div class="flex justify-between">
+        <TheButton
+          text="Отменить"
+          type="highlighted-red"
+          @click="cancelHandler"
+        />
+
+        <TheButton
+          :disabled="!isFormValid"
+          text="Подтвердить"
+          type="confirm"
+          @click="submitHandler"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import TheButton from "./TheButton.vue";
 export default {
+  components: { TheButton },
   name: "OrderCall",
+  props: {
+    city: {
+      type: Object,
+      default: null,
+    },
+  },
   data() {
     return {
       name: "",
@@ -17,7 +46,37 @@ export default {
     isNameValid() {
       return this.name.length;
     },
+    isPhoneValid() {
+      const phoneRegex = /^\+?[7]{1}[0-9]{10}$/g;
+      return phoneRegex.test(this.phone);
+    },
+    isEmailValid() {
+      const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      return emailRegex.test(this.email);
+    },
+    isFormValid() {
+      return this.isNameValid && this.isPhoneValid && this.isEmailValid;
+    },
+  },
   methods: {
+    cancelHandler() {
+      this.name = "";
+      this.phone = "";
+      this.email = "";
+      this.$store.commit("changeVisibilityDialog", false);
+    },
+    submitHandler() {
+      const userData = {
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        city_id: this.city.city_id,
+      };
+      if (this.isNameValid && this.isPhoneValid && this.isEmailValid) {
+        this.$store.dispatch("submitUser", userData);
+      }
+      this.cancelHandler();
+    },
   },
 };
 </script>
