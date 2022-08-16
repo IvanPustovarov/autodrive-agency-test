378c902f978d7360f4aa81c2a0dcb51016d67473
WIllsnSmith <silverflash150@gmail.com>
2022-08-16 12:59:19 +0300

    initial components

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="bg-gray-50 h-full"
+  >
+    <div
+      class="
+        max-w-7xl
+        mx-auto
+        py-12
+        px-4
+        sm:px-6
+        lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between
+      "
+    >
+      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+      </div>
+      <dialog
+        class="backdrop:bg-gray-50 open:bg-white open:text-white modal-window"
+        :open="visibilityDialog"
+      >
+      </dialog>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    visibilityDialog() {
+      return this.$store.state.isShow;
+    },
+  },
+};
+</script>
+<style lang="scss">
+</style>
---
+++ src/components/OrderCall.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="city" class="flex flex-row mb-4 input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderCall",
+  data() {
+    return {
+      name: "",
+      phone: "",
+      email: "",
+    };
+  },
+  computed: {
+    isNameValid() {
+      return this.name.length;
+    },
+  methods: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
