ef83f35c112e185dc3e480a2813df492c869fef8
WIllsnSmith <silverflash150@gmail.com>
2022-08-16 21:09:52 +0300

    state & mutation in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,29 @@ import axios from 'axios';
 export default createStore({
   state: {
     isShow: false,
+    callbackForm: null,
+    cities: [
+    {
+      city_id: 1,
+      name: "Москва",
+      text: "Заказать в Москву"
+    },
+    {
+      city_id: 2,
+      name: "Санкт-Петербург",
+      text: "Заказать в Санкт-Петербург"
+    }
+  ]
   },
   getters: {
   },
   mutations: {
+    changeVisibilityDialog(state, show) {
+      state.isShow = show;
+    },
+    setCallbackForm(state, payload) {
+      state.callbackForm = payload;
+    }
   },
   actions: {
   },
